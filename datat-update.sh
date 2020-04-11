b5a474fc4f4a573304d7a618e071388bd084b1bc
alaakh4192@gmail.com <Alaakh4192@gmail.com>
2020-04-11 18:10:57 +0300

    .

--- datat-update.sh
+++ datat-update.sh
@@ -1,3 +1,1 @@
-git clone https://github.com/Acadimics/data_update.git
-cd data_update
-npm i && npm start && node ./src/server.js
+git clone https://github.com/Acadimics/data_update.git && cd data_update && npm i && npm start && node ./src/server.js
